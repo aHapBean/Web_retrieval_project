d347e92efd00d8898b8897dbcf044e46d08cf20c
LUNAA77 <xuy_max@sjtu.du.cn>
2023-12-06 10:52:35 +0000

    rf

--- train.py
+++ train.py
@@ -1,11 +1,28 @@
 import subprocess
 import sys
 
-# python train.py svm
+# usage: python train.py svm
 PREFIX = 'python'
 MODEL = sys.argv[1]
 
-data_size_range = [10000, 20000, 50000, 100000, 200000]
 if MODEL == 'svm':
+    data_size_range = [10000, 20000, 50000, 100000, 200000]
     for DS in data_size_range:
         subprocess.run([PREFIX, 'svm.py', '--data_size', str(DS)])
+elif MODEL == 'random_forest':
+    data_size_range = [10000, 20000, 50000, 100000]
+    n_estimators_range = [100, 200, 500, 1000]
+    for NE in n_estimators_range:
+        for DS in data_size_range:
+            subprocess.run([PREFIX, 'random_forest.py',
+                           '--data_size', str(DS), '--n_estimators', str(NE)])
+elif MODEL == 'naive_bayes':
+    data_size_range = [10000, 20000, 50000, 100000]
+    for DS in data_size_range:
+        subprocess.run([PREFIX, 'naive_bayes.py', '--data_size', str(DS)])
+elif MODEL == 'knn':
+    data_size_range = [10000, 20000, 50000, 100000]
+    for DS in data_size_range:
+        subprocess.run([PREFIX, 'knn.py', '--data_size', str(DS)])
+else:
+    print("unexpected model name")
