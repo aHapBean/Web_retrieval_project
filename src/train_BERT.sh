423aa66501e64006297bec45e53d463fc0c5cb0c
aHapBean <2827276973@qq.com>
2023-12-15 10:52:11 +0800

    upd

--- src/train_BERT.sh
+++ src/train_BERT.sh
@@ -2,11 +2,10 @@
 # python BERT.py --lr 2e-5 > lr=2e-5.txt
 # python BERT.py --lr 1e-5 > lr=1e-5.txt
 
-python BERT.py --lr 1e-5 > 1e-5_new_washed_data.txt
-python BERT.py --lr 2e-5 > 2e-5_new_washed_data.txt
-python BERT.py --lr 3e-5 > 3e-5_new_washed_data.txt
-python BERT.py --lr 5e-5 > 5e-5_new_washed_data.txt
+python BERTmy.py --max-size 256 --gpu 2 > 256max-size_washed_data.txt
+python BERTmy.py --max-size 128 --gpu 2 > 128max-size_washed_data.txt
+python BERTmy.py --max-size 64 --gpu 2 > 64max-size_washed_data.txt
+python BERTmy.py --max-size 32 --gpu 2 > 32max-size_washed_data.txt
 
 # 练BERTmy.py
 # lr=4e-5
-
